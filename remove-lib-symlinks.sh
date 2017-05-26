e14f146ac706259a33d35021d680f49e970397a3
ntl <nathanladd+github@gmail.com>
2017-05-26 18:12:41 -0500

    Symlinks are corrected

--- remove-lib-symlinks.sh
+++ remove-lib-symlinks.sh
@@ -1,3 +1,3 @@
 source ./library-symlinks.sh
 
-remove_lib_symlinks 'entity_snapshot' 'postgres'
+remove_lib_symlinks 'postgres' 'entity_snapshot'
--- symlink-lib.sh
+++ symlink-lib.sh
@@ -1,3 +1,3 @@
 source ./library-symlinks.sh
 
-symlink_lib 'entity_snapshot' 'postgres'
+symlink_lib 'postgres' 'entity_snapshot'
