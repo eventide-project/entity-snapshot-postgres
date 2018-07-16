f41ffbd8b32cd835ceedcdd619bc2df143fc2edd
Scott Bellware <sbellware@gmail.com>
2018-07-16 02:02:04 -0500

    Vestigial serializer is removed

--- lib/entity_snapshot/postgres/recorded.rb
+++ lib/entity_snapshot/postgres/recorded.rb
@@ -4,15 +4,5 @@ module EntitySnapshot
 
     attribute :entity_data
     attribute :entity_version
-
-    module Serializer
-      def self.raw_data(instance)
-        instance.to_h
-      end
-
-      def self.instance(raw_data)
-        Message.build raw_data
-      end
-    end
   end
 end
