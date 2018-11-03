ffef2e6360e89f6cb7a48674585b46a1882d3de9
Scott Bellware <sbellware@gmail.com>
2018-11-03 17:57:51 -0500

    Library-level log tags are reduced to 'snapshot'

--- lib/entity_snapshot/postgres/log.rb
+++ lib/entity_snapshot/postgres/log.rb
@@ -2,9 +2,7 @@ module EntitySnapshot
   class Postgres
     class Log < ::Log
       def tag!(tags)
-        tags << :entity_snapshot_postgres
-        tags << :library
-        tags << :verbose
+        tags << :snapshot
       end
     end
   end
